aafc7df44102922f189e8582ac2d3aafb220cc8c
cici0702 <qdua0005@student.monash.edu>
2021-10-07 18:34:37 +0800

    add roxygon

---
+++ R/key_crop_yields.R
@@ -0,0 +1,18 @@
+
+#' The overall descriptions about the package
+#'
+#'
+#' @format
+#' \describe{
+#'   \item(entity)(countries)
+#'   \item(code)(code)
+#'   \item(year)(year,1961-2018)
+#'   \item(crop)(crops, wheat,rice,soybeans)
+#'   \item(tonnes_per_hectare)(hectare)}
+#'
+#' @source \url{https://github.com/cici0702/cwdata}
+#'
+#'
+#' @examples
+
+
